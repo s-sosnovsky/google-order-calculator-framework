52026d1dac71fbced7df8a241baa728ecfa55b3c
Sergey Sosnovsky <serge.sosnovsky@gmail.com>
2019-05-21 20:43:08 +0300

    Update CalculatorPage.java

--- src/test/java/com/epam/ta/page/googlecloud/CalculatorPage.java
+++ src/test/java/com/epam/ta/page/googlecloud/CalculatorPage.java
@@ -241,15 +241,15 @@ public class CalculatorPage extends AbstractPage {
     }
 
   public CalculatorPage setTenMinutesEmail(String email) throws InterruptedException {
-       ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,250)");
-        //new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until(ExpectedConditions.elementToBeClickable(emailValueField));
+       ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,500)");
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until(ExpectedConditions.elementToBeClickable(emailValueField));
         emailValueField.sendKeys(email);
         logger.info("Set 10 minutes email: " + email);
         return this;
     }
 
     public void clickSendEmailAddressButton() throws InterruptedException {
-        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,150)");
+        //((JavascriptExecutor)driver).executeScript("window.scrollBy(0,150)");
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS*2).until(ExpectedConditions.elementToBeClickable(sendEmailButton));
         sendEmailButton.click();
         logger.info("Email with estimated price sent");
