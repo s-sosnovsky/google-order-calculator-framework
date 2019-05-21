7a4680cdb9b27cde9169d1f6dbd5ff4321bd3d85
Sergey Sosnovsky <serge.sosnovsky@gmail.com>
2019-05-21 18:59:16 +0300

    21th

--- src/test/java/com/epam/ta/page/googlecloud/CalculatorPage.java
+++ src/test/java/com/epam/ta/page/googlecloud/CalculatorPage.java
@@ -241,6 +241,7 @@ public class CalculatorPage extends AbstractPage {
     }
 
   public CalculatorPage setTenMinutesEmail(String email) throws InterruptedException {
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,250)");
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until(ExpectedConditions.visibilityOf(emailValueField));
         emailValueField.sendKeys(email);
         logger.info("Set 10 minutes email: " + email);
