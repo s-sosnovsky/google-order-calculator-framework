b3f6cc3d2480ddd2bd734466dee695866fff9689
Sergey Sosnovsky <serge.sosnovsky@gmail.com>
2019-05-21 22:58:08 +0300

    Update CalculatorPage.java

--- src/test/java/com/epam/ta/page/googlecloud/CalculatorPage.java
+++ src/test/java/com/epam/ta/page/googlecloud/CalculatorPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.By;
+import com.epam.ta.model.Order;
 
 import java.util.List;
 
